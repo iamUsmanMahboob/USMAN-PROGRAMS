5921c53b37e2f166a6781fd287987de8e6b81013
Usman Mehboob <141616578+iamUsmanMahboob@users.noreply.github.com>
2023-08-13 23:03:08 +0500

    Add files via upload

---
+++ USMAN PROGRAM 6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	int result;
+	
+	cout<<"please Input First Value";
+	cin>>valueOne;
+	cout<<"please Input Second Value";
+	cin>>valueTwo;
+	
+	result=(valueOne-valueTwo)+(valueTwo+valueOne);
+	cout<<"The Result is";
+	cout<<result;
+	
+}
