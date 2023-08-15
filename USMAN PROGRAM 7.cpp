f38c02c245b121e7ee5ddd41c113d04bad39e913
Usman Mehboob <141616578+iamUsmanMahboob@users.noreply.github.com>
2023-08-15 20:07:34 +0500

    Add files via upload

---
+++ USMAN PROGRAM 7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int v=1; v<=10; v++)
+	{
+		int value1;
+		int value2;
+		cout<<"enter value 1";
+		cin>>value1;
+		cout<<"enter value 2";
+		cin>>value2;
+		cout<<value1+value2<<endl;
+	}		
+}
---
+++ USMAN PROGRAM 8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=1;
+	cout<<"please enter a number ";
+	cin>>i;
+	for(int j=1; j<=10;j++)
+	{
+	 cout<<i<<"x"<<j<<"="<<i*j<<endl;	
+	}
+}
